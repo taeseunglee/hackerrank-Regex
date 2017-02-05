15ed8d267771fbcc9dc8d57cf95854b0936b1274
taeseunglee <taeseunglee159@gmail.com>
2017-02-06 05:25:25 +0900

    very easy problem

---
+++ 7.Applications/HackerRank-Tweets.py
@@ -0,0 +1,11 @@
+import re
+
+num_lines = int(input())
+
+cnt = 0
+for nl in range(num_lines):
+    line = input()
+    if re.search(r'hackerrank', line, re.I):
+        cnt += 1
+        
+print(cnt)
