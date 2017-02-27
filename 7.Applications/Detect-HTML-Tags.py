786e90c9c1099a23ce76f79242c50698a240e54b
taeseunglee <taeseunglee159@gmail.com>
2017-02-27 23:52:12 +0900

    add if __name__ == "__main__":

--- 7.Applications/Detect-HTML-Tags.py
+++ 7.Applications/Detect-HTML-Tags.py
@@ -1,17 +1,18 @@
 import re
 
-regex_pattern = r'<\s*(\w+)'
-num_lines = int(input())
+if __name__ == "__main__":
+    regex_pattern = r'<\s*(\w+)'
+    num_lines = int(input())
 
-tag_list=[]
-for i in range(num_lines):
-    line = input()
-    splited_line = re.split('(<\s*(\w+))', line)
-    
-    for sub_line in splited_line:
-        match = re.search(regex_pattern, sub_line)
-        if match:
-            tag_list.append(str(match.group(1)))
+    tag_list=[]
+    for i in range(num_lines):
+        line = input()
+        splited_line = re.split('(<\s*(\w+))', line)
 
-tag_list = sorted(set(tag_list))
-print(";".join(tag_list))
+        for sub_line in splited_line:
+            match = re.search(regex_pattern, sub_line)
+            if match:
+                tag_list.append(str(match.group(1)))
+
+    tag_list = sorted(set(tag_list))
+    print(";".join(tag_list))
--- 7.Applications/Detect-HTML-links.py
+++ 7.Applications/Detect-HTML-links.py
@@ -1,18 +1,18 @@
 import re
+if __name__ == "__main__":
+    regex_pattern = r'a.*?href="([^"]*)".*?>(.*?)</a>'
+    num_lines=int(input())
 
-regex_pattern = r'a.*?href="([^"]*)".*?>(.*?)</a>'
-num_lines=int(input())
+    for i in range(num_lines):
+        line=input()
 
-for i in range(num_lines):
-    line=input()
+        splited_line = re.split('(<a.*?href="[^"]*".*?>.*?</a>)', line)
 
-    splited_line = re.split('(<a.*?href="[^"]*".*?>.*?</a>)', line)
-    
-    for sub_line in splited_line:
-        match = re.search(regex_pattern, sub_line)
+        for sub_line in splited_line:
+            match = re.search(regex_pattern, sub_line)
 
-        if match:
-            href = str(match.group(1))
-            text = str(match.group(2))
-            text = str(re.sub('<[^>]+>', '', text)).lstrip()
-            print(href + "," + text)
+            if match:
+                href = str(match.group(1))
+                text = str(match.group(2))
+                text = str(re.sub('<[^>]+>', '', text)).lstrip()
+                print(href + "," + text)
